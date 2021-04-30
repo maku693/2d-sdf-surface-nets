8f6e1f5116cb7e463fc3d9052929c6f27cd58073
maku693 <me@maku693.net>
2021-04-30 10:01:42 +0900

    Checkpoint

--- main.js
+++ main.js
@@ -4,14 +4,13 @@ const sdfdata = new SDFData(64, 64);
 const scene = merge(circle(32, 32, 16), circle(48, 48, 8));
 sdfdata.drawDistanceFunction(scene);
 
-const canvas = document.createElement("canvas");
-document.body.appendChild(canvas);
-canvas.width = sdfdata.width;
-canvas.height = sdfdata.height;
-
-const ctx = canvas.getContext("2d");
-
-function draw() {
+{
+  // Debug view
+  const canvas = document.createElement("canvas");
+  document.body.appendChild(canvas);
+  canvas.width = sdfdata.width;
+  canvas.height = sdfdata.height;
+  const ctx = canvas.getContext("2d");
   const imagedata = ctx.getImageData(0, 0, sdfdata.width, sdfdata.height);
   for (let y = 0; y < sdfdata.height; y++) {
     for (let x = 0; x < sdfdata.width; x++) {
@@ -26,10 +25,56 @@ function draw() {
   ctx.putImageData(imagedata, 0, 0);
 }
 
-draw();
+const squareEdges = [0, 1, 0, 2, 1, 3, 2, 3];
+
+/**
+ * cornersToEdges is edges indexed by corners.
+ * edges and corners are binary flag bits.
+ * a bit in corners represents which corners are in the volume.
+ * a bit in edges represents which edges are adjacent to the corner.
+ */
+const cornersToEdges = new Uint8Array(1 << 4);
+{
+  for (let i = 0; i < cornersToEdges.length; i++) {
+    let edges = 0;
+    for (let j = 0; j < 4; j++) {
+      const a = !!(i & (1 << squareEdges[j * 2]));
+      const b = !!(i & (1 << squareEdges[j * 2 + 1]));
+      edges |= a === b ? 0 : 1 << j;
+    }
+    cornersToEdges[i] = edges;
+  }
+}
+
+const pixelsPerGrid = 4;
+const canvas = document.createElement("canvas");
+document.body.appendChild(canvas);
+canvas.width = sdfdata.width * pixelsPerGrid;
+canvas.height = sdfdata.height * pixelsPerGrid;
+const ctx = canvas.getContext("2d");
+
+const corners = new Float32Array(4);
+for (let y = 0; y < sdfdata.height - 1; y++) {
+  for (let x = 0; x < sdfdata.width - 1; x++) {
+    let cornerMask = 0;
+    for (let v = 0; v < 2; v++) {
+      for (let u = 0; u < 2; u++) {
+        const d = sdfdata.data[x + u + (y + v) * sdfdata.width];
+        const i = u + v * 2;
+        corners[i] = d;
+        cornerMask |= (0 < d) << i;
+      }
+    }
+
+    const edges = cornersToEdges[cornerMask];
+    if (!edges) continue;
+
+    ctx.fillRect(x * pixelsPerGrid - 1, y * pixelsPerGrid - 1, 2, 2);
+  }
+}
 
 canvas.addEventListener("mousemove", (e) => {
-  if (e.buttons !== 1) return;
-  sdfdata.drawDistanceFunction(circle(e.offsetX, e.offsetY, 4));
-  draw();
+  //   if (e.buttons !== 1) return;
+  //   sdfdata.drawDistanceFunction(circle(e.offsetX, e.offsetY, 4));
+  //   draw();
 });
