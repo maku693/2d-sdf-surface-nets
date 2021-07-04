858508a23864cb255ef4695f02ee9a7eeee86664
maku693 <me@maku693.net>
2021-07-04 15:37:17 +0900

    Checkpoint

--- sdf.js
+++ sdf.js
@@ -13,10 +13,10 @@ export function circle(cx, cy, r) {
 export function draw(scene, data, numSamples, cellSize) {
   for (let v = 0; v < numSamples; v++) {
     for (let u = 0; u < numSamples; u++) {
+      // Shift the sampling point to center of the grid
       const x = (u + 0.5) * cellSize;
       const y = (v + 0.5) * cellSize;
       const i = u + numSamples * v;
-      // Shift the sampling point to center of the grid
       data[i] = Math.min(data[i], scene(x, y));
     }
   }
