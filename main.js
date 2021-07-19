518bc560ad34c6c2a668ff0917ddfc169d96d17e
maku693 <me@maku693.net>
2021-07-20 01:18:32 +0900

    Checkpoint

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ import init, {
 (async function main() {
   await init();
 
-  const width = 16;
+  const width = 32;
   const height = width;
   const data = new Float32Array(width * height).fill(Infinity);
 
@@ -47,7 +47,7 @@ import init, {
   }
   const { vertices, normals, indices } = geometryData;
 
-  const pixelsPerGrid = 20;
+  const pixelsPerGrid = 5;
   const canvas = document.getElementById("canvas");
   canvas.style.width = `${width * pixelsPerGrid}px`;
   canvas.style.height = `${height * pixelsPerGrid}px`;
@@ -55,82 +55,66 @@ import init, {
   canvas.height = height * pixelsPerGrid;
   const ctx = canvas.getContext("2d");
 
-  let mouseEvent;
-  function draw() {
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    for (let y = 0; y < height; y++) {
-      for (let x = 0; x < width; x++) {
-        const i = x + width * y;
-        const d = data[i];
-        const r = d > 0 ? 0xff * d : 0;
-        const g = d < 0 ? 0xff * -d : 0;
-        ctx.fillStyle = `rgba(${r}, ${g}, 0, 1)`;
-        ctx.strokeStyle = "black";
-        ctx.lineWidth = 1;
-        ctx.beginPath();
-        ctx.rect(
-          x * pixelsPerGrid,
-          y * pixelsPerGrid,
-          pixelsPerGrid,
-          pixelsPerGrid
-        );
-        ctx.fill();
-        ctx.stroke();
-        ctx.fillStyle = "white";
-        ctx.textBaseline = "middle";
-        ctx.textAlign = "center";
-        ctx.font = `${pixelsPerGrid * 0.4}px monospace`;
-        ctx.fillText(
-          `${d.toFixed(1)}`,
-          x * pixelsPerGrid + pixelsPerGrid / 2,
-          y * pixelsPerGrid + pixelsPerGrid / 2
-        );
-      }
-    }
-
-    ctx.lineWidth = 2;
-
-    for (let i = 0; i < indices.length / 2; i++) {
-      ctx.strokeStyle = "white";
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      const i = x + width * y;
+      const d = data[i];
+      const r = d > 0 ? 0xff * d : 0;
+      const g = d < 0 ? 0xff * -d : 0;
+      ctx.fillStyle = `rgba(${r}, ${g}, 0, 1)`;
+      ctx.strokeStyle = "black";
+      ctx.lineWidth = 1;
       ctx.beginPath();
-      ctx.moveTo(
-        vertices[indices[i * 2 + 0] * 2 + 0] * pixelsPerGrid,
-        vertices[indices[i * 2 + 0] * 2 + 1] * pixelsPerGrid
+      ctx.rect(
+        x * pixelsPerGrid,
+        y * pixelsPerGrid,
+        pixelsPerGrid,
+        pixelsPerGrid
       );
-      ctx.lineTo(
-        vertices[indices[i * 2 + 1] * 2 + 0] * pixelsPerGrid,
-        vertices[indices[i * 2 + 1] * 2 + 1] * pixelsPerGrid
-      );
+      ctx.fill();
       ctx.stroke();
-    }
-
-    for (let i = 0; i < vertices.length / 2; i++) {
-      ctx.strokeStyle = "cyan";
-      ctx.beginPath();
-      ctx.moveTo(
-        vertices[i * 2 + 0] * pixelsPerGrid,
-        vertices[i * 2 + 1] * pixelsPerGrid
+      ctx.fillStyle = "white";
+      ctx.textBaseline = "middle";
+      ctx.textAlign = "center";
+      ctx.font = `${pixelsPerGrid * 0.4}px monospace`;
+      ctx.fillText(
+        `${d.toFixed(1)}`,
+        x * pixelsPerGrid + pixelsPerGrid / 2,
+        y * pixelsPerGrid + pixelsPerGrid / 2
       );
-      ctx.lineTo(
-        (vertices[i * 2 + 0] + normals[i * 2 + 0]) * pixelsPerGrid,
-        (vertices[i * 2 + 1] + normals[i * 2 + 1]) * pixelsPerGrid
-      );
-      ctx.stroke();
-    }
-
-    if (mouseEvent) {
-      ctx.strokeStyle = "yellow";
-      ctx.beginPath();
-      ctx.moveTo(mouseEvent.offsetX, mouseEvent.offsetY);
-      ctx.lineTo(width * 0.5 * pixelsPerGrid, height * 0.5 * pixelsPerGrid);
-      ctx.stroke();
     }
   }
 
-  canvas.addEventListener("mousemove", (e) => {
-    mouseEvent = e;
-    draw();
-  });
+  ctx.lineWidth = 2;
+
+  for (let i = 0; i < indices.length / 2; i++) {
+    ctx.strokeStyle = "white";
+    ctx.beginPath();
+    ctx.moveTo(
+      vertices[indices[i * 2 + 0] * 2 + 0] * pixelsPerGrid,
+      vertices[indices[i * 2 + 0] * 2 + 1] * pixelsPerGrid
+    );
+    ctx.lineTo(
+      vertices[indices[i * 2 + 1] * 2 + 0] * pixelsPerGrid,
+      vertices[indices[i * 2 + 1] * 2 + 1] * pixelsPerGrid
+    );
+    ctx.stroke();
+  }
+
+  for (let i = 0; i < vertices.length / 2; i++) {
+    ctx.strokeStyle = "cyan";
+    ctx.beginPath();
+    ctx.moveTo(
+      vertices[i * 2 + 0] * pixelsPerGrid,
+      vertices[i * 2 + 1] * pixelsPerGrid
+    );
+    ctx.lineTo(
+      (vertices[i * 2 + 0] + normals[i * 2 + 0]) * pixelsPerGrid,
+      (vertices[i * 2 + 1] + normals[i * 2 + 1]) * pixelsPerGrid
+    );
+    ctx.stroke();
+  }
 })();
